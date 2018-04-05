a721bc337ffa21498ce05dac788b1e4e10fe346c
bridge <brisge@debian.bridge>
2018-04-05 20:02:26 +0800

    修改例题3-10

--- lesson/chapter3/eg3-10/eg3-10.c
+++ lesson/chapter3/eg3-10/eg3-10.c
@@ -1,3 +1,4 @@
+/*方法一：
 #include<stdio.h>
 int main(){
     char c1,c2;
@@ -5,5 +6,16 @@ int main(){
     c2=c1+32;
     putchar(c2);
     putchar('\n');
+    return 0;
+}
+*/
+
+//方法二：
+#include<stdio.h>
+int main(){
+    char c1,c2;
+    c1=getchar();
+    c2=c1+32;
+    printf("%c\n",c2);
     return 0;
 }
